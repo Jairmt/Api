b411ba0f9affb6a08a002a5d0aa954c24179daf4
rolog12 <rolog12@gmail.com>
2018-08-18 17:19:15 -0500

    ingresar body al post por consol

--- api_client.rb
+++ api_client.rb
@@ -1,18 +1,19 @@
 
 
-puts "ingrese su nombre"
-nombre = gets.chomp
+puts "ingrese El cuerpo de la publicacion"
+Post = gets.chomp
 
 require 'faraday'
 
 client = Faraday.new(url:'http://localhost:3000') do |config|
     config.adapter Faraday.default_adapter
+    config.token_auth('8405eb1800100429ecb1be5a15dcf7ab')
 end
 
 response = client.post do |req|
-    req.url'/api/v1/users'
+    req.url'/api/v1/posts'
     req.headers['Content-type']='application/json'
-    req.body = '{"user":{"name":"'+nombre+'"}}'
+    req.body = '{"post":{"body":"'+Post+'"}}'
 end
 
 require 'oj'
